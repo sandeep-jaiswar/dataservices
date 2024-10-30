0f4353395ba08be8ae25f97a7a6a3b94c491710d
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-10-30 13:31:10 +0530

    feat: add spring actuator

---
+++ src/test/java/com/sandeepsecurities/dataservices/DataservicesApplicationTests.java
@@ -0,0 +1,27 @@
+package com.sandeepsecurities.dataservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DataservicesApplicationTests {
+
+	@Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnStatusUp() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+
+}
